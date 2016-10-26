3546b1ee1d529e4ee5160f8d49f779b3571d1461
lina <443070582@qq.com>
2016-10-26 17:09:40 +0800

    gai

--- app/src/main/java/com/mylina/hellogithub/MainActivity.java
+++ app/src/main/java/com/mylina/hellogithub/MainActivity.java
@@ -2,12 +2,18 @@ package com.mylina.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+      private View tv123;
 
       @Override
       protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+            tv123 = findViewById(R.id.textView);
+
       }
 }
