acd2192c19aad14792040e29fb7df495fd8ae390
lina <443070582@qq.com>
2016-10-26 17:15:17 +0800

    gai

--- app/src/main/java/com/mylina/hellogithub/MainActivity.java
+++ app/src/main/java/com/mylina/hellogithub/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
       protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-
             tv123 = findViewById(R.id.textView);
 
       }
